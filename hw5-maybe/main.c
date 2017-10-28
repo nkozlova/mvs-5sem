d805ceaf68078e2991187f6348b70760dc62b24d
nkozlova <nadysha1997@yandex.ru>
2017-10-29 00:25:45 +0300

    1111

--- hw5-maybe/main.c
+++ hw5-maybe/main.c
@@ -32,12 +32,13 @@ int main(int argc, char* argv[]) {
 
     MPI_Init(&argc, &argv);
 
-    int array[10];
+    int arr_send[a * b][N];
+    int arr_recv[a * b][N];
+    double p_send[a * b][N];
+    double p_recv[a * b][N];
     int rank, size;
 
-/*    int pos[a * b][l * l];
-    int particular[a * b][N * a * b];
-*/    int count[a * b];
+    int count[a * b];
     Particular parts[N * a * b];
 
     double ts1 = MPI_Wtime();
@@ -46,37 +47,55 @@ int main(int argc, char* argv[]) {
     MPI_Status Status;
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    /* Генерация начального положения частиц в каждой ячейке */
     if (rank == a * b) {
-        for (int i = 0; i < N * a * b; i++) {
-            int z = rand() % (l * l);
-            MPI_Isend(&z, 1, MPI_INT, i % (a * b), 1, MPI_COMM_WORLD, &Request);
+        for (int i = 0; i < a * b; i++) {
+            for (int j = 0; j < N; j++) {
+                arr_send[i][j] = rand() % (l * l);
+            }
+        }
+        for (int i = 0; i < a * b; i++) {
+            MPI_Isend(&arr_send[i], N, MPI_INT, i, 1, MPI_COMM_WORLD, &Request);
+        }
+    }
+
+    /* Раскладываем частицы по местам */
+    if (rank != a * b) {
+        count[rank] = N;
+        MPI_Recv(arr_recv[rank], N, MPI_INT, a * b, 1, MPI_COMM_WORLD, &Status);
+        for (int i = 0; i < N; i++) {
+            parts[rank * N + i].number = rank * N + i;
+            parts[rank * N + i].node = rank;
+            parts[rank * N + i].pos = arr_recv[rank][i];
+            parts[rank * N + i].step = 0;
+        }
+    }
+
+    /* Генерация n шагов для каждой частицы */
+ //   for (int k = 0; k < n; k++)
+    if (rank == a * b) {
+        for (int i = 0; i < a * b; i++) {
+            for (int j = 0; j < N; j++) {
+                p_send[i][j] = 1. * rand() / RAND_MAX;
+            }
+            MPI_Isend(&p_send[i], N, MPI_DOUBLE, i, 1, MPI_COMM_WORLD, &Request);
+        }
+    }
+
+    if (rank != a * b) {
+        MPI_Recv(p_recv[rank], N, MPI_DOUBLE, a * b, 1, MPI_COMM_WORLD, &Status);
+        for (int i = 0; i < N; i++) {
+            printf("%d - %f\n", rank, p_recv[rank][i]);
         }
     }
 
 
-    count[rank] = N;
-    for (int i = 0; i < N; i++) {
-        parts[rank * (a + b) + i].number = rank * (a + b) + i;
-        parts[rank * (a + b) + i].node = rank;
-        parts[rank * (a + b) + i].step = 0;
-
-        MPI_Recv(&parts[rank * (a + b) + i].pos, 1, MPI_INT, a * b, rank, MPI_COMM_WORLD,  &Status);
-    }
-  /*   printf("I am %d of %d\n", rank, size);
-    for (int j = 0; j < l * l; j++) {
-        pos[rank][j] = rank;
-        printf("%d ", pos[rank][j]);
-    }
-    printf("\n");
-*/
-
-
-
     double ts2 = MPI_Wtime();
+
     if (rank == 0) {
         fprintf(file, "%d %d %d %d %d %f %f %f %f %fs\n", l, a, b, n, N, p_l, p_r, p_u, p_d, ts2 - ts1);
     }
-
     if (rank < a * b) {
         fprintf(file, "%d: %d\n", rank, count[rank]);
     }
